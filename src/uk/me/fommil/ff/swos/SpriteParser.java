3c45b19f3d1a9820d4538368f9b59b422feeb26e
Sam Halliday <sam.halliday@gmail.com>
2009-12-12 01:21:54 +0000

    speed up of sprite parsing

--- src/uk/me/fommil/ff/swos/SpriteParser.java
+++ src/uk/me/fommil/ff/swos/SpriteParser.java
@@ -15,18 +15,17 @@
 package uk.me.fommil.ff.swos;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import uk.me.fommil.ff.Main;
@@ -50,7 +49,7 @@ import uk.me.fommil.ff.Main;
  * <li>{@code byte} - unknown</li>
  * <li>{@code short} - width, for visual cropping</li>
  * <li>{@code short} - nlines (height)</li>
- * <li>{@code short} - wquads (number of bytes / 8) in one line</li>
+ * <li>{@code short} - wquads (number of pixels / 8) in one line</li>
  * <li>{@code short} - the x centre</li>
  * <li>{@code short} - the x centre</li>
  * <li>{@code byte} - unknown</li>
@@ -58,16 +57,14 @@ import uk.me.fommil.ff.Main;
  * <li>{@code short} - sprite id</li>
  * <li>{@code byte[]} - encoded pixel data, {@code 8 * nlines * wquads} length.</li>
  * </ul>
- * The {@code byte[]} part is encoded as follows, with padding between each byte in the stream:-
+ * The {@code byte[]} part is encoded as follows, with zero padding between each byte in the stream:-
  * <ul>
- * <li>line is divided in 4 "planes", 0, 1, 2 and 3 (wquads * 8 / 4)</li>
+ * <li>line is divided in 4 "planes", 0, 1, 2 and 3</li>
  * <li>every pixel consists of four bits</li>
  * <li>every pixel consists of 1 bit out of each plane</li>
  * <li>first pixel consists of highest bits from plane 3, 2, 1, 0 respectively</li>
  * <li>second pixel consists of following bits, and so on</li>
- * <li>algorithm is going through the line, assembling pixels bit by bit to the right, till the end of the line</li>
  * </ul>
- * This process is repeated until all lines are processed.
  *
  * @author Samuel Halliday
  */
@@ -75,7 +72,7 @@ public class SpriteParser {
 
 	private static final Logger log = Logger.getLogger(SpriteParser.class.getName());
 	// order to read the sprite files
-	private static final List<String> ORDER = Lists.newArrayList("CHARSET.DAT", "SCORE.DAT", "TEAM1.DAT", "TEAM3.DAT", "GOAL1.DAT", "GOAL1.DAT", "BENCH.DAT");
+	private static final List<String> ORDER = Lists.newArrayList("CHARSET.DAT", "SCORE.DAT", "TEAM1.DAT", "TEAM2.DAT", "TEAM3.DAT", "GOAL1.DAT", "BENCH.DAT");
 
 	/**
 	 * @param args
@@ -91,6 +88,7 @@ public class SpriteParser {
 					try {
 						dat.readInt();
 					} catch (EOFException e) {
+						log.info("read " + name);
 						break;
 					}
 					dat.skipBytes(6);
@@ -100,20 +98,24 @@ public class SpriteParser {
 					int wquads = Short.reverseBytes(dat.readShort());
 					dat.skipBytes(6);
 					int id = Short.reverseBytes(dat.readShort());
+
 					assert id >= 0 && id < 1335 : id;
-					log.info("nlines = " + nlines + ", wquads = " + wquads + ", width = " + width + ", id = " + id);
+					//log.info("nlines = " + nlines + ", wquads = " + wquads + ", width = " + width + ", id = " + id);
 					byte[] data = new byte[8 * wquads * nlines];
 					dat.readFully(data);
 					int[][] pixels = decodeSprite(data, wquads, nlines);
 
 					List<Color> pal = id >= 1209 && id <= 1272 ? SwosUtils.getGamePalette() : SwosUtils.getMenuPalette();
 
+					// TODO: use colour model
 					BufferedImage image = new BufferedImage(width, nlines, BufferedImage.TYPE_INT_RGB);
 					for (int y = 0; y < nlines; y++) {
 						for (int x = 0; x < width; x++) {
 							if (x >= 8 * wquads)
 								continue;
 							int c = pixels[y][x];
+							if (c == 0)
+								continue;
 							Color col = pal.get(c);
 							int rgb = col.getRGB();
 							image.setRGB(x, y, rgb);
@@ -127,18 +129,17 @@ public class SpriteParser {
 		}
 	}
 
-	static private int[][] decodeSprite(byte[] data, int wquads, int nlines) {
+	private static int[][] decodeSprite(byte[] data, int wquads, int nlines) {
 		int[] sprite = SwosUtils.unsignedBytesToInts(data);
 		int[][] output_pixels = new int[nlines][wquads * 8];
 		for (int lineIndex = 0; lineIndex < nlines; lineIndex++) {
-			// TODO remove need for copy
-			int[] line = Arrays.copyOfRange(sprite, wquads * 8 * lineIndex, wquads * 8 * (lineIndex + 1));
 			int output_pix_index = 0;
 			for (int byte_index = 0; byte_index < wquads; byte_index++) {
-				int byte1 = line[byte_index];
-				int byte2 = line[byte_index + 2];
-				int byte3 = line[byte_index + 4];
-				int byte4 = line[byte_index + 6];
+				int offset = wquads * 8 * lineIndex;
+				int byte1 = sprite[offset + byte_index];
+				int byte2 = sprite[offset + byte_index + 2];
+				int byte3 = sprite[offset + byte_index + 4];
+				int byte4 = sprite[offset + byte_index + 6];
 				for (int i = 0; i < 8; i++) {
 					int pixel = (byte1 & 0x80) >> 7;
 					pixel |= (byte2 & 0x80) >> 6;
