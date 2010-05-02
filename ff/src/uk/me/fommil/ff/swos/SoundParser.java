f372bebe1c1e5c527e1f89e6022362f0ea525c82
Sam Halliday <sam.halliday@gmail.com>
2010-05-02 16:04:47 +0000

    better index for commentary effects

--- ff/src/uk/me/fommil/ff/swos/SoundParser.java
+++ ff/src/uk/me/fommil/ff/swos/SoundParser.java
@@ -14,11 +14,15 @@
  */
 package uk.me.fommil.ff.swos;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
@@ -35,108 +39,74 @@ public class SoundParser {
 
 	private static final AudioFormat SWOS_RAW_FORMAT = new AudioFormat(22000, 8, 1, false, true);
 
-	enum Commentary {
+	/**
+	 * Index for various types of commentary sound files.
+	 */
+	public enum Commentary {
 
-		GOALS, ON_THE_POST, NEAR_MISSES, OWN_GOALS, CLAIMED_BY_THE_KEEPER,
-		SAVES_BY_THE_KEEPER, PENALTIES, SCORED_PENALTIES, MISSED_PENALTIES, SAVED_PENALTIES,
-		GOOD_PASSES, GOOD_PLAY, FOULS, GOOD_JUMPING_HEADERS, DISPUTED_REFEREE
+		GOALS("/SFX/PIERCE/", "M158_1_", "M158_2_", "M158_3_", "M158_4_", "M158_5_", "M158_7_"),
+		ON_THE_POST("/SFX/PIERCE/", "M10_f_", "M10_g_", "M10_i_", "M10_s_", "M10_t_", "M10_u_"),
+		NEAR_MISSES("/SFX/PIERCE/", "M10_d_", "M10_e_", "M10_j_", "M10_k_", "M10_r_"),
+		OWN_GOALS("/SFX/PIERCE/", "M158_p_", "M158_q_", "M158_s_", "M158_t_", "M158_x_", "M158_y_"),
+		CLAIMED_BY_THE_KEEPER("/SFX/PIERCE/", "M313_6_", "M313_8_", "M313_9_", "M313_c_", "M313_d_", "M313_g_"),
+		SAVES_BY_THE_KEEPER("/SFX/PIERCE/", "M196_w_", "M196_x_", "M196_z_", "M233_1_", "M313_7_", "M313_a_", "M313_b_", "M313_h_"), // "M313_e_" bad sampling in original)
+		PENALTIES("/SFX/PIERCE/", "M196_k_", "M196_l_", "M196_m_", "M196_n_"),
+		SCORED_PENALTIES("/SFX/PIERCE/", "M233_7_", "M233_8_", "M233_9_", "M233_c_"),
+		MISSED_PENALTIES("/SFX/PIERCE/", "M233_2_", "M233_4_", "M233_5_", "M233_6_"),
+		SAVED_PENALTIES("/SFX/PIERCE/", "M196_t_", "M196_u_", "M196_v_"),
+		GOOD_PASSES("/SFX/PIERCE/", "M313_i_", "M313_j_", "M313_o_"),
+		GOOD_PLAY("/SFX/PIERCE/", "M349_4_", "M349_7_", "M349_8_", "M349_e_", "M365_1_"),
+		FOULS("/SFX/PIERCE/", "M158_z_", "M195", "M196_1_", "M196_2_", "M196_4_", "M196_6_", "M196_7_"),
+		GOOD_JUMPING_HEADERS("/SFX/PIERCE/", "M33_2_", "M33_3_", "M33_4_", "M33_5_", "M33_6_", "M33_8_", "M33_a_", "M33_b_"),
+		DISPUTED_REFEREE("/SFX/PIERCE/", "M158__"),
+		TACTICS("/HARD/", "M10_5_", "M10_7_", "M10_8_", "M10_9_", "M10_a_", "M10_b_"),
+		SUBSTITUTE("/HARD/", "M233_j_", "M233_k_", "M233_l_", "M233_m_", "M10_3_", "M10_4_"),
+		THROW_IN("/HARD/", "M406_8_", "M406_9_", "M406_3_", "M406_7_"),
+		CORNERS("/HARD/", "M10_v_", "M10_w_", "M10_x_", "M10_y_", "M10_z_", "M313_1_", "M313_2_", "M313_3_"),
+		YELLOW_CARD("/HARD/", "M443_7_", "M443_8_", "M443_9_", "M443_a_", "M443_b_", "M443_c_", "M443_d_", "M443_e_", "M443_f_", "M443_g_", "M443_h_", "M443_i_", "M443_j_"),
+		RED_CARD("/HARD/", "M196_8_", "M196_9_", "M196_a_", "M196_b_", "M196_c_", "M196_d_", "M196_e_", "M196_f_", "M196_g_", "M196_h_", "M196_i_", "M196_j_"),
+		END_GAME("/HARD/", "M406_f_", "M406_g_", "M406_h_", "M406_i_", "M406_j_");
 
+		private final String directory;
+
+		private final String[] filenames;
+
+		private Commentary(String directory, String... filenames) {
+			this.directory = directory;
+			this.filenames = filenames;
+		}
+
+		/**
+		 * @return the filename of the sounds files associated to this commentary, relative to the
+		 * SWOS installation.
+		 */
+		List<String> getFilenames() {
+			List<String> names = Arrays.asList(filenames);
+			Iterable<String> fullNames = Iterables.transform(names, new Function<String, String>() {
+
+				@Override
+				public String apply(String from) {
+					return directory + from + ".RAW";
+				}
+			});
+			return Lists.newArrayList(fullNames);
+		}
 	}
 
-	private static final Multimap<Commentary, String> commentary = ArrayListMultimap.create();
+	enum ManagerVoiceover {
+		// FIXME: document the SFX/ENGLISH files, used for voice overs in the management game
+	}
 
-	static {
-		commentary.put(Commentary.GOALS, "M158_1_.raw");
-		commentary.put(Commentary.GOALS, "M158_2_.raw");
-		commentary.put(Commentary.GOALS, "M158_3_.raw");
-		commentary.put(Commentary.GOALS, "M158_4_.raw");
-		commentary.put(Commentary.GOALS, "M158_5_.raw");
-		commentary.put(Commentary.GOALS, "M158_7_.raw");
+	enum Flc {
+		// FIXME: document the SFX/FLC files, used for ambient sounds tied to results
+	}
 
-		commentary.put(Commentary.ON_THE_POST, "M10_f_.raw");
-		commentary.put(Commentary.ON_THE_POST, "M10_g_.raw");
-		commentary.put(Commentary.ON_THE_POST, "M10_i_.raw");
-		commentary.put(Commentary.ON_THE_POST, "M10_s_.raw");
-		commentary.put(Commentary.ON_THE_POST, "M10_t_.raw");
-		commentary.put(Commentary.ON_THE_POST, "M10_u_.raw");
+	enum Fx {
+		// FIXME: document the FX files, used for sound effects during play
 
-		commentary.put(Commentary.NEAR_MISSES, "M10_d_.raw");
-		commentary.put(Commentary.NEAR_MISSES, "M10_e_.raw");
-		commentary.put(Commentary.NEAR_MISSES, "M10_j_.raw");
-		commentary.put(Commentary.NEAR_MISSES, "M10_k_.raw");
-		commentary.put(Commentary.NEAR_MISSES, "M10_r_.raw");
-
-		commentary.put(Commentary.OWN_GOALS, "M158_p_.raw");
-		commentary.put(Commentary.OWN_GOALS, "M158_q_.raw");
-		commentary.put(Commentary.OWN_GOALS, "M158_s_.raw");
-		commentary.put(Commentary.OWN_GOALS, "M158_t_.raw");
-		commentary.put(Commentary.OWN_GOALS, "M158_x_.raw");
-		commentary.put(Commentary.OWN_GOALS, "M158_y_.raw");
-
-		commentary.put(Commentary.CLAIMED_BY_THE_KEEPER, "M313_6_.raw");
-		commentary.put(Commentary.CLAIMED_BY_THE_KEEPER, "M313_8_.raw");
-		commentary.put(Commentary.CLAIMED_BY_THE_KEEPER, "M313_9_.raw");
-		commentary.put(Commentary.CLAIMED_BY_THE_KEEPER, "M313_c_.raw");
-		commentary.put(Commentary.CLAIMED_BY_THE_KEEPER, "M313_d_.raw");
-		commentary.put(Commentary.CLAIMED_BY_THE_KEEPER, "M313_g_.raw");
-
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M196_w_.raw");
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M196_x_.raw");
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M196_z_.raw");
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M233_1_.raw");
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M313_7_.raw");
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M313_a_.raw");
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M313_b_.raw");
-		commentary.put(Commentary.SAVES_BY_THE_KEEPER, "M313_h_.raw");
-		// files.put(Commentary.SAVES_BY_THE_KEEPER, "M313_e_.raw"); // bad sampling in original
-
-		commentary.put(Commentary.PENALTIES, "M196_k_.raw");
-		commentary.put(Commentary.PENALTIES, "M196_l_.raw");
-		commentary.put(Commentary.PENALTIES, "M196_m_.raw");
-		commentary.put(Commentary.PENALTIES, "M196_n_.raw");
-
-		commentary.put(Commentary.SCORED_PENALTIES, "M233_7_.raw");
-		commentary.put(Commentary.SCORED_PENALTIES, "M233_8_.raw");
-		commentary.put(Commentary.SCORED_PENALTIES, "M233_9_.raw");
-		commentary.put(Commentary.SCORED_PENALTIES, "M233_c_.raw");
-
-		commentary.put(Commentary.MISSED_PENALTIES, "M233_2_.raw");
-		commentary.put(Commentary.MISSED_PENALTIES, "M233_4_.raw");
-		commentary.put(Commentary.MISSED_PENALTIES, "M233_5_.raw");
-		commentary.put(Commentary.MISSED_PENALTIES, "M233_6_.raw");
-
-		commentary.put(Commentary.SAVED_PENALTIES, "M196_t_.raw");
-		commentary.put(Commentary.SAVED_PENALTIES, "M196_u_.raw");
-		commentary.put(Commentary.SAVED_PENALTIES, "M196_v_.raw");
-
-		commentary.put(Commentary.GOOD_PASSES, "M313_i_.raw");
-		commentary.put(Commentary.GOOD_PASSES, "M313_j_.raw");
-		commentary.put(Commentary.GOOD_PASSES, "M313_o_.raw");
-
-		commentary.put(Commentary.GOOD_PLAY, "M349_4_.raw");
-		commentary.put(Commentary.GOOD_PLAY, "M349_7_.raw");
-		commentary.put(Commentary.GOOD_PLAY, "M349_8_.raw");
-		commentary.put(Commentary.GOOD_PLAY, "M349_e_.raw");
-		commentary.put(Commentary.GOOD_PLAY, "M365_1_.raw");
-
-		commentary.put(Commentary.FOULS, "M158_z_.raw");
-		commentary.put(Commentary.FOULS, "M195.raw");
-		commentary.put(Commentary.FOULS, "M196_1_.raw");
-		commentary.put(Commentary.FOULS, "M196_2_.raw");
-		commentary.put(Commentary.FOULS, "M196_4_.raw");
-		commentary.put(Commentary.FOULS, "M196_6_.raw");
-		commentary.put(Commentary.FOULS, "M196_7_.raw");
-
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_2_.raw");
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_3_.raw");
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_4_.raw");
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_5_.raw");
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_6_.raw");
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_8_.raw");
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_a_.raw");
-		commentary.put(Commentary.GOOD_JUMPING_HEADERS, "M33_b_.raw");
-
-		commentary.put(Commentary.DISPUTED_REFEREE, "M158__.raw");
+		AWAYSUBL, BOUNCEX, CHANT8L, COMBROWN, COMWHITE, ENDGAMEW, FOUL, HOMEGOAL, KICKX, ONENIL, TWONIL,
+		BGCRD3L, CHANT10L, CHEER, COMGREEN, COMYELO, EREWEGO, FOURNIL, HOMESUBL, MISSGOAL, REF_WH, WHISTLE,
+		BOOWHISL, CHANT4L, COMBLUE, COMRED, EASY, FIVENIL, GOALKICK, HOMEWINL, NOTSING, THREENIL,
 	}
 
 	/**
@@ -144,11 +114,11 @@ public class SoundParser {
 	 * @throws Exception
 	 */
 	public static final void main(String[] args) throws Exception {
-//		File file = new File(Main.SWOS + "/SFX/PIERCE/M196_K_.RAW"); // "penalty" SFX
-		//File file = new File(Main.SWOS + "/HARD/CHEER.RAW");
+		for (String filename : Commentary.END_GAME.getFilenames()) {
+			File file = new File(Main.SWOS + filename);
 
-		for (String filename : commentary.get(Commentary.GOOD_JUMPING_HEADERS)) {
-			File file = new File(Main.SWOS + "/SFX/PIERCE/" + filename);
+			System.out.println(file);
+			Preconditions.checkArgument(file.exists());
 			InputStream in = new FileInputStream(file);
 
 			AudioInputStream audio = new AudioInputStream(in, SWOS_RAW_FORMAT, file.length());
